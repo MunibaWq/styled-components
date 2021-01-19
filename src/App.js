b05f031807d495264035d5efef18e0df74df20d5
Muniba Waqar <muniba.wq@gmail.com>
2021-01-19 12:33:28 -0700

    first style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
+const Title = styled.h1`
+  color: red;
+  font-size: 25rem;
+`;
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Title>My Styled Title</Title>
+    </div>
+  );
 };
 
 export default App;
